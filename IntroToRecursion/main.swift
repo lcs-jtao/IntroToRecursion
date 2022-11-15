7b9edf11e7844a1fe0f915acc30c806f30891426
Joyce Tao <jtao@lcs.on.ca>
2022-11-15 09:21:10 -0500

    iteration and recursion example

--- IntroToRecursion/main.swift
+++ IntroToRecursion/main.swift
@@ -7,5 +7,40 @@
 
 import Foundation
 
-print("Hello, World!")
+// Iteration: for-in
+for i in 1...10 {
+    print(i)
+}
+print("I can count to ten! 🎉")
 
+print("---")
+
+// Iteration: while
+var i = 1
+while i <= 10 {
+    print(i)
+    i += 1
+}
+print("I can also count to ten! 🥳")
+
+print("---")
+
+// Recursion
+// Define the recursive function
+func countUp(with j: inout Int) {
+    
+    print(j)
+    j += 1
+    
+    if j <= 10 {
+        countUp(with: &j)
+    } else {
+        // All done (base case is j equals 11)
+        print("I can count to ten in what feels like an overly complicated way! 🤓🎈")
+    }
+}
+
+// Invoke the recursive function
+var currentValue = 1
+countUp(with: &currentValue)
+print("After the function has run, currentValue is: \(currentValue)")
